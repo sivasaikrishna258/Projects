152c6873f86e05adafc97fe01a5017c115ee3d32
Sai Prathap <Sai Prathap@LAPTOP-CD3D46K5>
2024-02-29 17:02:25 +0530

    new commit

--- HybridFramewok/src/test/java/com/CRM/qa/Testcases/Roughwork.java
+++ HybridFramewok/src/test/java/com/CRM/qa/Testcases/Roughwork.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
@@ -33,15 +34,11 @@ public class Roughwork {
 		driver.findElement(By.name("email")).sendKeys("sivasaikrishna258@gmail.com");
 		driver.findElement(By.name("password")).sendKeys("Sivasai@123");
 		driver.findElement(By.xpath("//div[contains(@class,'ui fluid large blue submit button')]")).click();
-		 WebElement calendarlink=driver.findElement(By.xpath("//*[contains(text(),'Calendar')]"));
+		 driver.findElement(By.xpath("//*[contains(text(),'Advanapu  Sivasaikrishna ')]")).isDisplayed();
+		 Assert.assertTrue(true, "username is displayed");
 
-		new WebDriverWait(driver,   
-					Duration.ofSeconds(30)).until(ExpectedConditions.visibilityOf(calendarlink)).click();
+		
 		 
-		driver.findElement(By.xpath("//button[contains(text(),'Events')]")).click();
-		 WebElement calendarlink1=driver.findElement(By.xpath("//input[contains(text(),'meeting')]"));
-		new WebDriverWait(driver,   
-				Duration.ofSeconds(30)).until(ExpectedConditions.visibilityOf(calendarlink1)).click();
 
 		//*[contains(text(),'meeting')]
 	}
